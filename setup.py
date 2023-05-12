a7c751c131ad931761eaf0d52a6e2b838fde7f01
hiroto22 <tochihirototo.nya.49@gmail.com>
2023-05-13 02:34:25 +0900

    :tada: ディレクトリ構成を作成

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using baysian-optimazation for cell free ',
+    author='Hiroto Yamazaki',
+    license='',
+)
