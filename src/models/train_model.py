a82b0557d920e34c0693e0371c5bb9ae8140291a
hiroto22 <tochihirototo.nya.49@gmail.com>
2023-05-26 18:30:03 +0900

    :construction: svr_gaussianの実装

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -65,4 +65,51 @@ def svr_linear(fold_number,autoscaled_x, autoscaled_y,x):
 
     return model
 
-    
+# サポートベクター回帰(ガウシアンカーネル)
+def svr_gaussian(fold_number,autoscaled_x, autoscaled_y,y):
+    nonlinear_svr_cs = 2 ** np.arange(-5, 10, dtype=float)  # SVR の C の候補
+    nonlinear_svr_epsilons = 2 ** np.arange(-10, 0, dtype=float)  # SVR の ε の候補
+    nonlinear_svr_gammas = 2 ** np.arange(-20, 10, dtype=float)  # SVR のガウシアンカーネルの γ の候補
+
+    # C, ε, γの最適化
+    # 分散最大化によるガウシアンカーネルのγの最適化
+    variance_of_gram_matrix = []
+    autoscaled_x_array = np.array(autoscaled_x)
+    for nonlinear_svr_gamma in nonlinear_svr_gammas:
+        gram_matrix = np.exp(- nonlinear_svr_gamma * ((autoscaled_x_array[:, np.newaxis] - autoscaled_x_array) ** 2).sum(axis=2))
+        variance_of_gram_matrix.append(gram_matrix.var(ddof=1))
+    optimal_nonlinear_gamma = nonlinear_svr_gammas[np.where(variance_of_gram_matrix==np.max(variance_of_gram_matrix))[0][0]]
+
+    cross_validation = KFold(n_splits=fold_number, random_state=9, shuffle=True) # クロスバリデーションの分割の設定
+    # CV による ε の最適化
+    r2cvs = [] # 空の list。候補ごとに、クロスバリデーション後の r2 を入れていきます
+    for nonlinear_svr_epsilon in nonlinear_svr_epsilons:
+        model = SVR(kernel='rbf', C=3, epsilon=nonlinear_svr_epsilon, gamma=optimal_nonlinear_gamma)
+        autoscaled_estimated_y_in_cv = cross_val_predict(model, autoscaled_x, autoscaled_y, cv=cross_validation)
+        r2cvs.append(r2_score(y, autoscaled_estimated_y_in_cv * y.std() + y.mean()))
+    optimal_nonlinear_epsilon = nonlinear_svr_epsilons[np.where(r2cvs==np.max(r2cvs))[0][0]] # クロスバリデーション後の r2 が最も大きい候補
+
+    # CV による C の最適化
+    r2cvs = [] # 空の list。候補ごとに、クロスバリデーション後の r2 を入れていきます
+    for nonlinear_svr_c in nonlinear_svr_cs:
+        model = SVR(kernel='rbf', C=nonlinear_svr_c, epsilon=optimal_nonlinear_epsilon, gamma=optimal_nonlinear_gamma)
+        autoscaled_estimated_y_in_cv = cross_val_predict(model, autoscaled_x, autoscaled_y, cv=cross_validation)
+        r2cvs.append(r2_score(y, autoscaled_estimated_y_in_cv * y.std() + y.mean()))
+    optimal_nonlinear_c = nonlinear_svr_cs[np.where(r2cvs==np.max(r2cvs))[0][0]] # クロスバリデーション後の r2 が最も大きい候補
+
+    # CV による γ の最適化
+    r2cvs = [] # 空の list。候補ごとに、クロスバリデーション後の r2 を入れていきます
+    for nonlinear_svr_gamma in nonlinear_svr_gammas:
+        model = SVR(kernel='rbf', C=optimal_nonlinear_c, epsilon=optimal_nonlinear_epsilon, gamma=nonlinear_svr_gamma)
+        autoscaled_estimated_y_in_cv = cross_val_predict(model, autoscaled_x, autoscaled_y, cv=cross_validation)
+        r2cvs.append(r2_score(y, autoscaled_estimated_y_in_cv * y.std() + y.mean()))
+    optimal_nonlinear_gamma = nonlinear_svr_gammas[np.where(r2cvs==np.max(r2cvs))[0][0]] # クロスバリデーション後の r2 が最も大きい候補
+    # 結果の確認
+    print('最適化された C : {0} (log(C)={1})'.format(optimal_nonlinear_c, np.log2(optimal_nonlinear_c)))
+    print('最適化された ε : {0} (log(ε)={1})'.format(optimal_nonlinear_epsilon, np.log2(optimal_nonlinear_epsilon)))
+    print('最適化された γ : {0} (log(γ)={1})'.format(optimal_nonlinear_gamma, np.log2(optimal_nonlinear_gamma)))
+    # モデル構築
+    model = SVR(kernel='rbf', C=optimal_nonlinear_c, epsilon=optimal_nonlinear_epsilon, gamma=optimal_nonlinear_gamma)  # SVR モデルの宣言
+    model.fit(autoscaled_x, autoscaled_y)
+
+    return model
